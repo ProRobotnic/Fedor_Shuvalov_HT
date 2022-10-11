42dcae3407b26ba813b2ab1c71076abc19c0c4da
Fedor Shuvalov <fedorsh2004@gmail.com>
2022-10-11 20:16:22 +0300

    Working ht3!

--- ht3/one_way_linked_list.c
+++ ht3/one_way_linked_list.c
@@ -25,7 +25,6 @@ struct one_way_linked_elem element(struct one_way_linked_list list, int position
     for (int i = 0; i < position; i++) {
         if (current_elem->next_elem != 0x100000000) {
             current_elem = current_elem->next_elem;
-
         }
     }
     return *current_elem;
@@ -33,28 +32,51 @@ struct one_way_linked_elem element(struct one_way_linked_list list, int position
 
 //Функция добавления элемента в односвязный список
 //Вход: односвязный список + значение новго элемента
-struct one_way_linked_list append(struct one_way_linked_list list, int number) {
-    struct one_way_linked_elem new_elem; // Создаем новый элемент
-    new_elem.value = number; // Присваеваем новому элементу значение
-    if (list.isnt_null != '1') {
-        list.first_elem = &new_elem;
-        printf("%i\n", list.first_elem->value);
-        list.isnt_null = '1';
 
+struct one_way_linked_list append(struct one_way_linked_list list, int number) {
+    struct one_way_linked_elem *new_elem = (struct one_way_linked_elem *)malloc(sizeof(struct one_way_linked_elem)); // Создаем новый элемент
+    new_elem->value = number; // Присваеваем новому элементу значение
+    if (list.isnt_null != 1) {
+        list.first_elem = new_elem;
+        list.isnt_null = 1;
+        list.last_elem = new_elem;
+    } else {
+        list.last_elem->next_elem = new_elem; // Указываем предыдущему элементу на новый
+        list.last_elem = new_elem;
     }
-    (*list.last_elem).next_elem = &new_elem; // Указываем предыдущему элементу на новый
-    list.last_elem = &new_elem;
     return list;
 };
 
 int main() {
     struct one_way_linked_list new_list;
+    new_list.isnt_null = '0';
     for (int i = 0; i < 10; i++) {
-        new_list = append(new_list, i);
-
+        /*struct one_way_linked_elem new_elem;// Создаем новый элемент
+        new_elem.value = i; // Присваеваем новому элементу значение
+        if (new_list.isnt_null != 1) {
+            new_list.first_elem = &new_elem;
+            new_list.isnt_null = 1;
+            new_list.last_elem = &new_elem;
+        } else {
+            new_list.last_elem->next_elem = &new_elem; // Указываем предыдущему элементу на новый
+            new_list.last_elem = &new_elem;
+        }*/
+        new_list = append(new_list,i);
     }
-    printf("%i\n", (*new_list.first_elem).value);
-    printf("%i\n", 1);
-    printf("%i\n", element(new_list, 4).value);
-    return 0;
+    for (int i = 0; i < 10; i++) {
+        printf("%i\n", element(new_list, i).value);
+    }
+    //printf("%i\n", (*new_list.first_elem).value);
+    //printf("%i\n", element(new_list, 4).value);
+    /*struct one_way_linked_list new_list;
+    struct one_way_linked_elem new_elem;
+    struct one_way_linked_elem new_elem_2;
+    new_list.first_elem = &new_elem;
+    new_list.last_elem = &new_elem;
+    new_list.last_elem->next_elem = &new_elem_2;
+    new_list.last_elem = &new_elem_2;
+    new_elem.value = 5;
+    new_elem_2.value = 4;
+    printf("%i\n", new_list.first_elem->next_elem->value);
+    return 0;*/
 }
--- ht3/two_way_linked_list.c
+++ ht3/two_way_linked_list.c
@@ -12,7 +12,8 @@ struct two_ways_linked_elem {
 // В нем храним первую переменную и последнюю
 // (Лень делать функцию нахождения последнего элемента)
 struct two_ways_linked_list {
-    struct two_ways_linked_elem last_elem;
+    char isnt_null;
+    struct two_ways_linked_elem *last_elem;
     struct two_ways_linked_elem *first_elem;
 };
 
@@ -20,25 +21,33 @@ struct two_ways_linked_list {
 
 //Функция добавления элемента в односвязный список
 //Вход: односвязный список + значение новго элемента
+
+
 struct two_ways_linked_list append(struct two_ways_linked_list list, int number) {
-    struct two_ways_linked_elem new_elem; // Создаем новый элемент
-    new_elem.value = number; // Присваеваем новому элементу значение
-    if (list.last_elem.next_elem == 0x100000000) {
-        list.first_elem = &new_elem;
+    struct two_ways_linked_elem *new_elem = (struct two_way_linked_elem *) malloc(
+            sizeof(struct two_ways_linked_elem)); // Создаем новый элемент
+    new_elem->value = number; // Присваеваем новому элементу значение
+    if (list.isnt_null != 1) {
+        list.first_elem = new_elem;
+        list.isnt_null = 1;
+        list.last_elem = new_elem;
+    } else {
+        list.last_elem->next_elem = new_elem; // Указываем предыдущему элементу на новый
+        new_elem->previous_elem = list.last_elem;
+        list.last_elem = new_elem;
+
     }
-    list.last_elem.next_elem = &new_elem; // Указываем предыдущему элементу на новый
-    new_elem.previous_elem = &list.last_elem; // И наоборот
-    list.last_elem = new_elem;
     return list;
-}
+};
 
 int main() {
     struct two_ways_linked_list new_list;
+    new_list.isnt_null = 0;
     for (int i = 0; i < 10; i++) {
         new_list = append(new_list, i);
 
     }
-    printf("%i", new_list.last_elem.value);
+    printf("%i", new_list.last_elem->value);
 
     return 0;
 }
